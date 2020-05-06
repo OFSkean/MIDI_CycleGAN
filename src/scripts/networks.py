45adc2bca1fbea48e403ffafa9a96ad36bef4edf
ofskean727 <ofskean727@gmail.com>
2020-05-05 22:07:33 -0400

    Defined a custom layer class for tying weights together Defined a method to insert custom layer into a model

--- src/scripts/networks.py
+++ src/scripts/networks.py
@@ -54,6 +54,62 @@ def define_discriminator(input_shape):
     model.compile(loss='mse', optimizer=Adam(lr=0.0002, beta_1=0.5), loss_weights=[0.5])
     return model
 
+# define a custom layer so that weights can be tied together
+class CustomLayer(Layer):
+    def __init__(self, tied_layer, output_shape, strides, activation=None, **kwargs):
+        self.tied_layer = tied_layer
+        self.out_shape = output_shape
+        self.strides = strides
+        self.activation = keras.activations.get(activation)
+        super().__init__(**kwargs)
+
+    def build(self, batch_input_shape):
+        self.biases = self.add_weight(name="bias",
+                                      shape=[self.tied_layer.input_shape[-1]],
+                                      initializer='ones')
+        super().build(batch_input_shape)
+
+    def call(self, inputs, **kwargs):
+        z = tf.nn.conv3d_transpose(inputs, self.tied_layer.weights[0], self.out_shape, self.strides)
+        return z
+
+    def compute_output_shape(self, input_shape):
+        print('shapes', (input_shape[0], self.out_shape))
+        return (input_shape[0], self.out_shape)
+
+# replaces an intermediate layer in a keras model with a tied layer
+def tie_weights(model, other_model, layer_id, other_id, num_samples):
+    from keras.models import Model
+
+    layers = [l for l in model.layers]
+
+    # create the layer that will facilitate the tying of weights
+    output_shape = list(model.layers[layer_id].output_shape)
+    output_shape[0] = -1
+    newLayer = CustomLayer(other_model.layers[other_id], tf.convert_to_tensor(output_shape), (2,2,2))
+
+    x = layers[0].output
+    mergeLayerIndices = list(range(9, 9, 6))
+    mergeLayerTemp = x
+
+    for i in range(1, len(layers)):
+        print(i)
+        if i == layer_id:
+            x = newLayer(x)
+
+        # handle merge layers
+        elif i in mergeLayerIndices:
+            if i == mergeLayerIndices[0]:
+                x = layers[i](x)
+            else:
+                x = keras.layers.Add()([x, mergeLayerTemp])
+            mergeLayerTemp = x
+        else:
+            print(x.shape)
+            x = layers[i](x)
+
+    new_model = Model(input=layers[0].input, output=x)
+    return new_model
 
 # define a resnet block
 def resnet_block(n_filters, input_layer):
